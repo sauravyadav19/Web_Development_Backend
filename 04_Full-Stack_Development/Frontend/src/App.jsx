e3c8ae763f80575a1eb87cded1dfc0a931905542
Saurav <sauravsaurav0@gmail.com>
2026-05-29 10:36:33 +0530

    04_Full-Stack_Development: Initialize a skeleton react project for frontend

---
+++ 04_Full-Stack_Development/Frontend/src/App.jsx
@@ -0,0 +1,7 @@
+export default function App(){
+
+    return(
+        <h2>This is App</h2>
+    )
+
+}
---
+++ 04_Full-Stack_Development/Frontend/src/main.jsx
@@ -0,0 +1,8 @@
+import {createRoot} from "react-dom/client"
+import App from "./App"
+
+// Finding an element that has Id of `root` in index.html and selecting it
+// and then using `createRoot` function and making this as our root element in react
+const root = createRoot(document.getElementById('root'))
+// the `root` will then render a component `App` inside it.
+root.render(<App/>)
