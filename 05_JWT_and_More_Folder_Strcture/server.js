fca53a42ade1c8de027fd9e7ff7026f68f5c4e96
Saurav <sauravsaurav0@gmail.com>
2026-06-16 13:33:42 +0530

    Integrate .env handling for server

--- 05_JWT_and_More_Folder_Strcture/server.js
+++ 05_JWT_and_More_Folder_Strcture/server.js
@@ -1,7 +1,17 @@
+// importing path to give absoulte paths rather than realtive paths (check module 04 for reference)
+import path from "path";
+const __dirname = import.meta.dirname
+const envPath = path.join(__dirname, ".env")
+
+// Importing and configuring dotenv to read .env file
+import dotenv from "dotenv";
+dotenv.config({path:envPath})
+
 // import Express Instance from App.js
 import app from "./src/app.js";
 
 // Start listening for incoming connnection on port sepcfified
-app.listen(5173,()=>{
-    console.log("Listening on Port 5173")
+// changing the port to be soureced from .env file rather than hardcoding (good practice)
+app.listen(process.env.PORT,()=>{
+    console.log(`Listening on Port ${process.env.PORT}`)
 })
