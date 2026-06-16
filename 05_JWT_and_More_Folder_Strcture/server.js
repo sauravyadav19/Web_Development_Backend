a17c9f92fa8eaef3144e27fa3006853be80ce383
Saurav <sauravsaurav0@gmail.com>
2026-06-16 13:20:23 +0530

    Create an Instance of Express and Listen for incoming connections

---
+++ 05_JWT_and_More_Folder_Strcture/server.js
@@ -0,0 +1,7 @@
+// import Express Instance from App.js
+import app from "./src/app.js";
+
+// Start listening for incoming connnection on port sepcfified
+app.listen(5173,()=>{
+    console.log("Listening on Port 5173")
+})
---
+++ 05_JWT_and_More_Folder_Strcture/src/app.js
@@ -0,0 +1,7 @@
+//Step-1 import Express
+import express from 'express';
+// Step 2: Instantiate Express
+const app = express()
+
+//Step 3: Export the Express Instance for use in other files.
+export default app;
